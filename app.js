f2b53fbf61244832aa3b95eb47b284dc433f90da
officialharikrishnan <harikrishnanpnr001@gmail.com>
2023-06-09 00:47:45 +0530

    cors cors added

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 import express from 'express'
 import { routes } from './routes/index.js'
 import dependencies from './config/dependencies.js'
+import cors from 'cors'
 const app = express()
 const router = express.Router()
 
+app.use(cors())
 app.use(express.json())
 
 app.use('/api',routes(dependencies))
