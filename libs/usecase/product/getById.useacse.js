6f50d5a49998464b9b01a3cfe726bc92604d55b3
officialharikrishnan <harikrishnanpnr001@gmail.com>
2023-07-05 10:25:03 +0530

    image bug

--- libs/usecase/product/getById.useacse.js
+++ libs/usecase/product/getById.useacse.js
@@ -1,3 +1,5 @@
+import { getFile } from "../../../config/s3.config.js";
+
 export const getByIdUseCase = (dependencies) => {
   const {
     repository: { productRepository },
@@ -6,8 +8,14 @@ export const getByIdUseCase = (dependencies) => {
     try {
       const product = await productRepository.getById(data);
       if (product) {
+        const getObjectParams = {
+            Bucket:process.env.S3_NAME,
+            Key:product.image
+        }
+        const url = await getFile(getObjectParams)
+        product.image=url
         return product;
-      } else {
+      } else { 
         return null;
       }
     } catch (e) {
--- libs/usecase/product/getSearchProduct.usecase.js
+++ libs/usecase/product/getSearchProduct.usecase.js
@@ -1,8 +1,19 @@
+import { getFile } from "../../../config/s3.config.js"
+
 export const getSearchProductUseCase = (dependencies)=>{
     
     const {repository:{productRepository}}=dependencies
     const execute =async (data)=>{
-        return await productRepository.search(data) 
+        const products  = await productRepository.search(data) 
+        for(let product of products){
+            const getObjectParams = {
+                Bucket:process.env.S3_NAME,
+                Key:product.image
+            }
+            const url = await getFile(getObjectParams)
+            product.image=url
+        }
+        return products
     }
     return {
         execute
